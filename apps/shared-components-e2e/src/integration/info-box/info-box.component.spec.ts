28821f9a7c003fc5ec7e0db5804b837918ba77df
Isaac Mann <isaacplmann+git@gmail.com>
2019-12-04 19:10:16 -0500

    add cypress specs

--- apps/shared-components-e2e/src/integration/info-box/info-box.component.spec.ts
+++ apps/shared-components-e2e/src/integration/info-box/info-box.component.spec.ts
@@ -1,9 +1,19 @@
 describe('shared-components', () => {
-  it('should render the component', () => {
+  beforeEach(() => {
+    cy.viewport(400, 400);
+  });
+  it('should show John', () => {
     cy.visit(
       '/iframe.html?id=infoboxcomponent--primary&knob-icon=person&knob-message=John'
     );
     cy.get('mat-icon').should('contain', 'person');
     cy.contains('John');
+  });
+  it('should show Bob', () => {
+    cy.visit(
+      '/iframe.html?id=infoboxcomponent--primary&knob-icon=person&knob-message=Bob'
+    );
+    cy.get('mat-icon').should('contain', 'person');
+    cy.contains('Bob');
   });
 });
